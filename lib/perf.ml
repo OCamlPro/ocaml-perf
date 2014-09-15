79d4665c6cbbbd64bbbac51752df6e8d1cc91d56
Vincent Bernardoff <vb@luminar.eu.org>
2014-09-15 18:10:37 +0200

    Initial commit.

---
+++ lib/perf.ml
@@ -0,0 +1,140 @@
+module Attr = struct
+  type flag =
+    | Disabled [@value 1] (** off by default *)
+    | Inherit [@value 2] (** children inherit it *)
+    | Exclude_user [@value 4] (** don't count user *)
+    | Exclude_kernel [@value 8] (** don't count kernel *)
+    | Exclude_hv [@value 16] (** don't count hypervisor *)
+    | Exclude_idle [@value 32] (** don't count when idle *)
+    | Enable_on_exec [@value 64] (** next exec enables *)
+        [@@deriving Enum]
+
+  type kind =
+    (** Hardware *)
+    | Cycles
+    | Instructions
+    | Cache_references
+    | Cache_misses
+    | Branch_instructions
+    | Branch_misses
+    | Bus_cycles
+    | Stalled_cycles_frontend
+    | Stalled_cycles_backend
+    | Ref_cpu_cycles
+
+    (** Software *)
+    | Cpu_clock
+    | Task_clock
+    | Page_faults
+    | Context_switches
+    | Cpu_migrations
+    | Page_faults_min
+    | Page_faults_maj
+    | Alignment_faults
+    | Emulation_faults
+    | Dummy
+        [@@deriving Enum]
+
+  type t = {
+    flags: flag list;
+    kind: kind
+  }
+  (** Opaque type of a perf event attribute. *)
+
+  let make ?(flags=[]) kind = { flags; kind; }
+  (** [make ?flags kind] is a perf event attribute of type [kind],
+      with flags [flags]. *)
+end
+
+type flag =
+  | Fd_cloexec [@value 1]
+  | Fd_no_group [@value 2]
+  | Fd_output [@value 4]
+  | Pid_cgroup [@value 8]
+      [@@deriving Enum]
+
+type t = {
+  fd: Unix.file_descr;
+  kind: Attr.kind;
+}
+
+external perf_event_open : int -> int -> int -> int -> int ->
+  int -> Unix.file_descr = "stub_perf_event_open_byte" "stub_perf_event_open"
+external perf_event_ioc_enable : Unix.file_descr -> unit = "perf_event_ioc_enable"
+external perf_event_ioc_disable : Unix.file_descr -> unit = "perf_event_ioc_disable"
+external perf_event_ioc_reset : Unix.file_descr -> unit = "perf_event_ioc_reset"
+
+external enable_all : unit -> unit = "perf_events_enable_all"
+external disable_all : unit -> unit = "perf_events_disable_all"
+
+module FlagSet = Set.Make(struct type t = flag let compare = compare end)
+module AttrFlagSet = Set.Make(struct type t = Attr.flag let compare = compare end)
+
+let make ?(pid = 0) ?(cpu = -1) ?group ?(flags = []) attr =
+  let flags = FlagSet.(of_list flags |> elements) in
+  let flags = List.fold_left (fun acc f -> acc + flag_to_enum f) 0 flags in
+
+  let attr_flags = AttrFlagSet.(of_list attr.Attr.flags |> elements) in
+  let attr_flags = List.fold_left
+      Attr.(fun acc f -> acc + Attr.(flag_to_enum f)) 0 attr_flags in
+
+  let group = match group with
+    | None -> -1
+    | Some { fd; _ } -> (Obj.magic fd : int) in
+  let kind_enum = Attr.(kind_to_enum attr.kind) in
+  Attr.{ fd = perf_event_open kind_enum attr_flags pid cpu group flags;
+         kind = attr.kind;
+       }
+
+let kind c = c.kind
+
+let read c =
+  let buf = Bytes.create 8 in
+  let nb_read = Unix.read c.fd buf 0 8 in
+  assert (nb_read = 8);
+  Int64.of_string buf
+
+let reset c = perf_event_ioc_reset c.fd
+let enable c = perf_event_ioc_enable c.fd
+let disable c = perf_event_ioc_disable c.fd
+
+type result = {
+  measures: (Attr.kind * int64) list;
+  stdout: string;
+  stderr: string;
+}
+
+let string_of_channel ic =
+  let buf = Buffer.create 10 in
+  let bytes = Bytes.create 4096 in
+  let rec drain () =
+    match input ic bytes 0 4096 with
+    | 0 -> Buffer.contents buf
+    | n -> Buffer.add_subbytes buf bytes 0 n; drain ()
+  in drain ()
+
+let with_process ~attrs ~cmd =
+  let attrs = List.map Attr.(fun a ->
+      { flags = [Disabled; Inherit; Enable_on_exec] @ a.flags;
+        kind = a.kind
+      }) attrs in
+  let counters = List.map make attrs in
+  let chd_stdout_pipe_in, chd_stdout_pipe_out = Unix.pipe () in
+  let chd_stderr_pipe_in, chd_stderr_pipe_out = Unix.pipe () in
+  let chd_stdout = Unix.in_channel_of_descr chd_stdout_pipe_in in
+  let chd_stderr = Unix.in_channel_of_descr chd_stderr_pipe_in in
+  match Unix.fork () with
+  | 0 ->
+      (* child *)
+      Unix.(dup2 chd_stdout_pipe_out stdout);
+      Unix.(dup2 chd_stderr_pipe_out stderr);
+      Unix.execvp (List.hd cmd) (Array.of_list cmd)
+  | n ->
+      (* parent *)
+      let _ = Unix.waitpid [] 0 in
+      List.iter disable counters;
+      {
+        measures = List.map (fun c -> c.kind, read c) counters;
+        stdout = string_of_channel chd_stdout;
+        stderr = string_of_channel chd_stderr;
+      }
---
+++ lib/perf.mli
@@ -0,0 +1,137 @@
+module Attr : sig
+  type flag =
+    | Disabled (** off by default *)
+    | Inherit (** children inherit it *)
+    | Exclude_user (** don't count user *)
+    | Exclude_kernel (** don't count kernel *)
+    | Exclude_hv (** don't count hypervisor *)
+    | Exclude_idle (** don't count when idle *)
+    | Enable_on_exec (** next exec enables *)
+
+  type kind =
+    (** Hardware *)
+    | Cycles
+    | Instructions
+    | Cache_references
+    | Cache_misses
+    | Branch_instructions
+    | Branch_misses
+    | Bus_cycles
+    | Stalled_cycles_frontend
+    | Stalled_cycles_backend
+    | Ref_cpu_cycles
+
+    (** Software *)
+    | Cpu_clock
+    | Task_clock
+    | Page_faults
+    | Context_switches
+    | Cpu_migrations
+    | Page_faults_min
+    | Page_faults_maj
+    | Alignment_faults
+    | Emulation_faults
+    | Dummy
+
+  type t
+  (** Opaque type of a perf event attribute. *)
+
+  val make : ?flags:flag list -> kind -> t
+  (** [make ?flags kind] is a perf event attribute of type [kind],
+      with flags [flags]. *)
+end
+
+type flag =
+  | Fd_cloexec (** (since Linux 3.14). This flag enables
+                             the close-on-exec flag for the created
+                             event file descriptor, so that the file
+                             descriptor is automatically closed on
+                             execve(2).  Setting the close-on-exec
+                             flags at creation time, rather than later
+                             with fcntl(2), avoids potential race
+                             conditions where the calling thread
+                             invokes perf_event_open() and fcntl(2) at
+                             the same time as another thread calls
+                             fork(2) then execve(2). *)
+
+
+  | Fd_no_group (** This flag allows creating an event as
+                              part of an event group but having no
+                              group leader.  It is unclear why this is
+                              useful.*)
+
+  | Fd_output (** This flag reroutes the output from an event to the group leader. *)
+
+  | Pid_cgroup (** This flag activates per-container
+                             system-wide monitoring.  A container is
+                             an abstraction that isolates a set of
+                             resources for finer-grained control
+                             (CPUs, memory, etc.).  In this mode, the
+                             event is measured only if the thread
+                             running on the monitored CPU belongs to
+                             the designated container (cgroup).  The
+                             cgroup is identified by passing a file
+                             descriptor opened on its directory in the
+                             cgroupfs filesystem.  For instance, if
+                             the cgroup to monitor is called test,
+                             then a file descriptor opened on
+                             /dev/cgroup/test (assuming cgroupfs is
+                             mounted on /dev/cgroup) must be passed as
+                             the pid parameter.  cgroup monitoring is
+                             available only for system-wide events and
+                             may therefore require extra
+                             permissions. *)
+
+type t
+(** Opaque type of an event counter (internally [t] is a file
+       descriptor). Each file descriptor corresponds to one event that
+       is measured; these can be grouped together to measure multiple
+       events simultaneously. *)
+
+val make : ?pid:int -> ?cpu:int -> ?group:t -> ?flags:flag list -> Attr.t -> t
+(** [make ~pid ~cpu ?group ?flags attr] is a perf event counter. Refer
+    to perf_event_open(2) for the description of the arguments. One
+    counter only counts one kind of attribute at a time. If you want
+    to simultanously count different metrics (like the perf stat tool
+    does), you have to setup several counters. *)
+
+val kind : t -> Attr.kind
+(** [kind c] is the kind of events that this counter counts. *)
+
+val read : t -> Int64.t
+(** [read c] is the value of the counter [c]. *)
+
+val reset : t -> unit
+(** [reset c] sets [c] to zero. *)
+
+val enable : t -> unit
+(** Start measuring. *)
+
+val disable : t -> unit
+(** Disabling an event. When an event is disabled it does not count or
+       generate overflows but does continue to exist and maintain its
+       count value. *)
+
+type result = private {
+  measures: (Attr.kind * int64) list;
+  stdout: string;
+  stderr: string;
+}
+(** Type returned by [with_process] *)
+
+val with_process : attrs:Attr.t list ->
+  cmd:string list -> result
+(** [with_process ~attrs ~cmd] is an association list containing the
+    results from the measurement of [~attrs] on the execution of the
+    program described by [~cmd]. *)
+
+val enable_all : unit -> unit
+(** A process can enable or disable all the event groups that are
+     attached to it using the prctl(2) PR_TASK_PERF_EVENTS_ENABLE and
+     PR_TASK_PERF_EVENTS_DISABLE operations.  This applies to all
+     counters on the calling process, whether created by this process
+     or by another, and does not affect any counters that this process
+     has created on other processes.  It enables or disables only the
+     group leaders, not any other members in the groups. *)
+
+val disable_all : unit -> unit
